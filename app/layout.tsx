528964ce1e2a30df03bb00fd60e3d6d939d63452
Andrea Frazier <contact@rosewaystudio.com>
2026-02-23 16:12:41 -0500

    add favicon and site.manifest

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,6 +19,16 @@ export const metadata: Metadata = {
     url: "https://rosewaystudio.com",
     siteName: "Roseway Studio",
   },
+  icons: {
+    icon: [
+      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+    ],
+    apple: [
+      { url: '/apple-touch-icon.png', sizes: '180x180' },
+    ],
+  },
+  manifest: '/site.webmanifest',
 };
 
 export default function RootLayout({
